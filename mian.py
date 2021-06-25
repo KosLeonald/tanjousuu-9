c9159218e4789a8cfc549c445e91a61b968f9b8f
KosLeonald <75104470+KosLeonald@users.noreply.github.com>
2021-06-25 16:58:21 +0900

    Update mian.py

--- mian.py
+++ mian.py
@@ -14,10 +14,10 @@ def main():
     mylist = ["大吉","中吉","小吉","凶","大凶"]
     messages1= TextSendMessage(random.choice(mylist))
     messages= TextSendMessage(text=f"今日のあなたの運勢は、{random.choice(mylist)}です。" )
-    messages2= TextSendMessage(text = "です。")
+    messages2= TextSendMessage(text = "今日も1日頑張りましょう♪")
     line_bot_api.push_message(USER_ID,messages=messages)
    # line_bot_api.push_message(USER_ID,messages=messages1)
-   # line_bot_api.push_message(USER_ID,messages=messages2)
+     line_bot_api.push_message(USER_ID,messages=messages2)
     
 if __name__ == "__main__" :
     main()
