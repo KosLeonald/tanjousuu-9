c9ecf003728d430e24c14e9f4f6ca6bf0529401e
KosLeonald <75104470+KosLeonald@users.noreply.github.com>
2021-06-26 02:00:25 +0900

    Update mian.py

--- mian.py
+++ mian.py
@@ -15,16 +15,10 @@ def main():
     USER_ID = info['USER_ID']
     mylist = ["大吉","中吉","小吉","凶","大凶"]
     messages1= TextSendMessage(random.choice(mylist))
-    messages= TextSendMessage(text=f"今日のあなたの運勢は、{random.choice(mylist)}です。" )
+    messages = TextSendMessage(text=f"今日のあなたの運勢は、{random.choice(mylist)}です。" )
     messages2= TextSendMessage(text = "今日も1日頑張りましょう♪")
   
- today = datetime.datetime.fromtimestamp(time.time())
- time  = today.strftime('%H')  
- 
- if int(time) > 20 :
-    line_bot_api.push_message(USER_ID,messages=messages)
- else :
-    line_bot_api.push_message(USER_ID,messages=messages2)
+
     
 if __name__ == "__main__" :
     main()
