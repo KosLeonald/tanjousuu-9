93cdbdaa5e8e962ef6acbb072cb8a33438080cf9
KosLeonald <75104470+KosLeonald@users.noreply.github.com>
2021-06-25 15:58:17 +0900

    Update mian.py

--- mian.py
+++ mian.py
@@ -13,7 +13,7 @@ def main():
     USER_ID = info['USER_ID']
     mylist = ["大吉","中吉","小吉","凶","大凶"]
     messages1= TextSendMessage(random.choice(mylist))
-    messages= TextSendMessage(text="今日のあなたの運勢は" + random.choice(mylist) + text="です。" /n + text = "今日も一日頑張ろう！")
+    messages= TextSendMessage(text="今日のあなたの運勢は" + random.choice(mylist) + text = "です。" )
     line_bot_api.push_message(USER_ID,messages=messages)
     line_bot_api.push_message(USER_ID,messages=messages1)
     
