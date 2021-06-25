8c42d00666b67cd1407c4a5577c73e07783a755c
KosLeonald <75104470+KosLeonald@users.noreply.github.com>
2021-06-25 15:20:11 +0900

    Update mian.py

--- mian.py
+++ mian.py
@@ -11,7 +11,7 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
-    messages1= TextSendMessage(random.randint(1,3))
+    messages1= TextSendMessage(random.randint("aka","ao"))
     messages= TextSendMessage(text="あと少しだよ、がんばって～")
     line_bot_api.push_message(USER_ID,messages=messages)
     line_bot_api.push_message(USER_ID,messages=messages1)
