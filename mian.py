b16dc4a1c38059662c32e25012105bcafa17d0b0
KosLeonald <75104470+KosLeonald@users.noreply.github.com>
2021-06-25 15:55:15 +0900

    Update mian.py

--- mian.py
+++ mian.py
@@ -11,9 +11,9 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 def main():
     USER_ID = info['USER_ID']
-    mylist = ["赤","青","黄","紫"]
+    mylist = ["大吉","中吉","小吉","凶","大凶"]
     messages1= TextSendMessage(random.choice(mylist))
-    messages= TextSendMessage(text="あと少しだよ、がんばって～")
+    messages= TextSendMessage(text="今日のあなたの運勢は" + random.choice(mylist) + text="です。" /n + text = "今日も一日頑張ろう！")
     line_bot_api.push_message(USER_ID,messages=messages)
     line_bot_api.push_message(USER_ID,messages=messages1)
     
